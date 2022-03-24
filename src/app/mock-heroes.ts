055af825fab2f0ad44946a1bbe83b74d947fd18d
AbrahamPP2000 <abraham.ppinedo@alumnos.udg.mx>
2022-03-24 14:39:49 -0600

    Segundo intento con Angular

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Saitama', points: 10000 },
+  { id: 12, name: 'Genos', points: 9000 },
+  { id: 13, name: 'Tatsumaki', points: 8000 },
+  { id: 14, name: 'Silver Fang', points: 7000 },
+  { id: 15, name: 'Flashy Flash', points: 6000 },
+  { id: 16, name: 'Watchdog Man', points: 5000 },
+  { id: 17, name: 'Metal Bat', points: 4000 },
+  { id: 18, name: 'TankTop Master', points: 3000 },
+  { id: 19, name: 'Fubuki', points: 2000 },
+  { id: 20, name: 'Mumen Rider', points: 1000 }
+];
